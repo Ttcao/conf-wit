e4a6ae087cb9f04d513afcf420c7e3f12f5a49bc
Tya Cao <cao.tya@gmail.com>
2017-01-29 23:31:17 +1100

    add socket.io functionality

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const request = require('request');
 const PORT = 3000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 // const WIT_TOKEN = process.env.WIT_TOKEN;
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('Send msg to Wit: ' + msg);
+
+    // Call Wit Ai code
+
+    Wit.client.runActions('test-session', msg, {})
+      .then(function(data) {
+        // console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+        // Send Wit's response to the UI
+        socket.emit('chat response', data.timetable);
+      })
+      .catch(console.error);
+
+
+
+  });
+
+});
 
 const Wit = require('./examples/quickstart.js');
 // const {interactive} = require('./examples/quickstart.js');
@@ -15,7 +40,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // creates a route
-app.get('/chat/:msg', function(request, response) {
+app.get('/message/:msg', function(request, response) {
   response.send('Hello World from Express!');
   // console.log(Wit.client)
   Wit.client.runActions('test-session', request.params.msg, {})
@@ -23,12 +48,12 @@ app.get('/chat/:msg', function(request, response) {
       console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
     })
     .catch(console.error);
-  });
+});
 
-// app.get('/chat', function(request, response) {
-//   response.render('chat', { message: 'Welcome to the chat page!' });
-// });
+app.get('/chat', function(request, response) {
+  response.render('chat', { message: 'Welcome to the chat page!' });
+});
 
-app.listen(PORT, function() {
+http.listen(PORT, function() {
   console.log('Example app listening on port ' + PORT);
 });
