e625267c6ab68f2ee5bcf79caae1c02390bbf808
Tya Cao <cao.tya@gmail.com>
2017-01-30 16:27:06 +1100

    add some changes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const request = require('request');
-const PORT = 3000;
+const PORT = 4567;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const Wit = require('./examples/quickstart.js');
+// const {interactive} = require('./examples/quickstart.js');
+// interactive(client);
 
 // const WIT_TOKEN = process.env.WIT_TOKEN;
 
@@ -14,7 +17,6 @@ io.on('connection', function(socket){
     console.log('Send msg to Wit: ' + msg);
 
     // Call Wit Ai code
-
     Wit.client.runActions('test-session', msg, {})
       .then(function(data) {
         // console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
@@ -23,15 +25,10 @@ io.on('connection', function(socket){
       })
       .catch(console.error);
 
-
-
   });
 
 });
 
-const Wit = require('./examples/quickstart.js');
-// const {interactive} = require('./examples/quickstart.js');
-// interactive(client);
 
 // the views are going to be stored in a directory called "views"
 app.set('views', './views');
@@ -40,18 +37,18 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // creates a route
-app.get('/message/:msg', function(request, response) {
-  response.send('Hello World from Express!');
-  // console.log(Wit.client)
-  Wit.client.runActions('test-session', request.params.msg, {})
-    .then(function(data) {
-      console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
-    })
-    .catch(console.error);
-});
+// app.get('/message/:msg', function(request, response) {
+//   response.send('Hello World from Express!');
+//   // console.log(Wit.client)
+//   Wit.client.runActions('test-session', request.params.msg, {})
+//     .then(function(data) {
+//       console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+//     })
+//     .catch(console.error);
+// });
 
 app.get('/chat', function(request, response) {
-  response.render('chat', { message: 'Welcome to the chat page!' });
+  response.render('chat', { message: 'Ask our bot!' });
 });
 
 http.listen(PORT, function() {
